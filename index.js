d1c07359d32ad9556aeb5f1f496357e6e052010f
martin0123478 <martin_agui97@outlook.es>
2022-02-27 12:13:04 -0600

    creando primer ruta

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express = require('express');
 //crear el servidor de expres
 const app = express();
 
-
+//rutas
+app.get('/', (req,res) =>{
+  res.json({
+    ok:true,
+    msg:'Hola mundo'
+  })
+});
 app.listen(3000, () =>{
   console.log('Servidos corriendo en puerto ' + 3000)
 })
