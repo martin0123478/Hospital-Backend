e0a050ea9f57756a349ccaf20ccb44b3c76b4ef7
martin0123478 <martin_agui97@outlook.es>
2022-03-16 21:52:19 -0600

    validación de email duplicado

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,3 +1,4 @@
+const {response} = require('express')
 
 const Usuario = require('../models/usuario')
 const getUsuarios = async(req,res) =>{
@@ -11,13 +12,30 @@ const getUsuarios = async(req,res) =>{
 
 const crearUsuario = async (req,res) =>{
   const {email,password,nombre} = req.body;
-  const usuario = new Usuario(req.body);
+  try {
+    const existeEmail = await Usuario.findOne({email});
+    if(existeEmail){
+      return res.status(400).json({
+        ok:false,
+        msg:'el correo ya existe'
+      })
+    }
+    const usuario = new Usuario(req.body);
   await usuario.save();
-
   res.json({
     ok:true,
     usuario
   })
+  } catch (error) {
+    
+    res.status(500).json({
+      ok:false,
+      msg:'Error inesperado'
+    })
+  }
+  
+
+  
 }
 
 module.exports = {
