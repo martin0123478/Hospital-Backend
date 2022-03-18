04ada801de838ceb468d04df4a3323af62575e36
martin0123478 <martin_agui97@outlook.es>
2022-03-18 17:03:37 -0600

    actualización de datos a usuarios, validación de email

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,6 +1,7 @@
 const {response} = require('express')
 
 const Usuario = require('../models/usuario')
+const bcrypt = require('bcryptjs')
 const getUsuarios = async(req,res) =>{
   const usuarios =await Usuario.find({}, 'nombre email role google')
   
@@ -22,6 +23,12 @@ const crearUsuario = async (req,res) =>{
       })
     }
     const usuario = new Usuario(req.body);
+
+    //encriptar contraseña
+
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password,salt)
+    //guardar usuario
   await usuario.save();
   res.json({
     ok:true,
@@ -39,7 +46,52 @@ const crearUsuario = async (req,res) =>{
   
 }
 
+const actualizarUsuario = async (req,res=response) =>{
+  try {
+    
+    const uid = req.params.id
+    const usuarioDB = await Usuario.findById(uid);
+    if(!usuarioDB){
+      return res.status(404).json({
+        ok:false,
+        msg:'Ese usuario no existe'
+      })
+    }
+    //Actualizaciones
+    const  campos = req.body;
+    if(usuarioDB.email === req.body.email){
+      delete campos.email
+    }else{
+      const existeEmail = await Usuario.findOne({email:req.body.email});
+      if(existeEmail){
+        return res.status(400).json({
+          ok:false,
+          msg:'Ya existe es usuario con ese email'
+        })
+      }
+    }
+
+
+
+    delete campos.password;
+    delete campos.google;
+
+    const usuarioActualizado = await Usuario.findByIdAndUpdate(uid,campos,{new:true});
+    res.json({
+      ok:true,
+      usuario:usuarioActualizado
+    })
+    
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok:false,
+      msg:'error inesperado'
+    })
+  }
+}
 module.exports = {
   getUsuarios,
-  crearUsuario
+  crearUsuario,
+  actualizarUsuario
 }
--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,7 +3,7 @@ ruta : /api/usuarios
 */
 
 const {Router} = require('express')
-const {getUsuarios,crearUsuario} = require('../controllers/usuario')
+const {getUsuarios,crearUsuario,actualizarUsuario} = require('../controllers/usuario')
 const {check} = require('express-validator')
 
 const {validarCampos} = require('../middlewares/validar-campos')
@@ -19,5 +19,12 @@ router.post('/',
       ],
  crearUsuario );
 
+router.put('/:id',
+    [
+      check('nombre','El nombre es obligatorio').not().isEmpty(),
+      check('email','El email es obligatorio').isEmail(),
+      check('role','El rol es obligatorio').isEmail(),
+  ],
 
+        actualizarUsuario);
 module.exports = router;
